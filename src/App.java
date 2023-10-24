83e057f1637b4df862c111e0c686f2a213ccee41
edvnsn2609 <edvensonmiguel@gmail.com>
2023-10-24 14:56:53 +0800

    :sparkles: feat; login page

--- src/App.java
+++ src/App.java
@@ -1,7 +1,30 @@
 
+import Client.Admin.LoginPage;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 public class App {
 
-    public static void main(String[] args[]) {
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
 
+//                MyConnection con = new MyConnection();
+//                con.checkConnection();
+
+//                if (con.isConnected == true) {
+//                    LoginPage lg = new LoginPage();
+//
+//                    lg.setVisible(true);
+//                   
+//                } else {
+//                    JOptionPane.showMessageDialog(null, "Error connecting to database.", "Connection failed.", JOptionPane.ERROR_MESSAGE);
+//                }
+                    LoginPage lg = new LoginPage();
+
+                    lg.setVisible(true);
+            }
+        });
     }
 }
+
---
+++ src/Server/Jpanel.java
@@ -0,0 +1,46 @@
+package Server;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Jpanel {
+
+    public void viewFrame(String className, String title) {
+        try {
+            JFrame frame = new JFrame(title);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+            Class<?> clazz = Class.forName(className);
+            Object loginPanel = clazz.getDeclaredConstructor().newInstance();
+
+            if (loginPanel instanceof JPanel) {
+                JPanel panel = (JPanel) loginPanel;
+                panel.setPreferredSize(new Dimension(1000, 600));
+
+                frame.getContentPane().add(panel);
+                frame.pack();
+
+                
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                int screenWidth = screenSize.width;
+                int screenHeight = screenSize.height;
+
+                
+                int xPos = (screenWidth - frame.getWidth()) / 2;
+                int yPos = (screenHeight - frame.getHeight()) / 2;
+
+             
+                frame.setLocation(xPos, yPos);
+
+                frame.setVisible(true);
+            } else {
+                System.out.println("Class specified is not a JPanel.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
