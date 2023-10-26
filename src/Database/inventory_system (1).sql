3bbe994fdf4a077f2deb1e994e51434dec0f089d
SunnyVirgo <sunny.virgo@antiquespride.edu.ph>
2023-10-26 20:48:16 +0800

    update sale table in database

--- src/Database/inventory_system (1).sql
+++ src/Database/inventory_system (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2023 at 04:58 AM
+-- Generation Time: Oct 26, 2023 at 02:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -57,15 +57,26 @@ CREATE TABLE `sale` (
   `date` date NOT NULL,
   `project_or_end_user` varchar(50) NOT NULL,
   `quantity` int(10) NOT NULL,
-  `active_components` date NOT NULL,
+  `description` varchar(255) NOT NULL,
   `supplier` varchar(100) NOT NULL,
-  `supplier_price` decimal(10,0) NOT NULL,
-  `srp` decimal(10,0) NOT NULL,
+  `supplier_price` decimal(10,2) NOT NULL,
+  `srp` decimal(10,2) NOT NULL,
   `remarks` varchar(255) NOT NULL,
   `date_accomplished` date NOT NULL,
   `last_update` date NOT NULL,
-  `deadline` date NOT NULL
+  `deadline` date NOT NULL,
+  `user_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sale`
+--
+
+INSERT INTO `sale` (`sale_id`, `date`, `project_or_end_user`, `quantity`, `description`, `supplier`, `supplier_price`, `srp`, `remarks`, `date_accomplished`, `last_update`, `deadline`, `user_id`) VALUES
+(1, '2023-10-27', 'hehehe', 23, 'shaa', 'ako', 23.00, 543.00, 'df,s', '2023-10-27', '2023-10-27', '2023-10-21', 6),
+(2, '2023-10-27', 'fkersd', 324, 'dfjs', 'sdfd', 43.00, 45.00, 'gfdxs', '2023-10-27', '2023-10-27', '2023-10-27', 4),
+(3, '2023-10-27', 'hehehe', 10, 'dfjs', 'sdfd', 43.00, 45.00, 'gfdxs', '2023-10-27', '2023-10-27', '2023-10-27', 4),
+(4, '2023-10-19', 'hahaha', 232, 'gedff', 'fekjrs', 32.00, 45.00, 'gvd,f', '2023-10-20', '2023-10-21', '2023-10-20', 4);
 
 -- --------------------------------------------------------
 
@@ -91,11 +102,18 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `user_type`, `last_name`, `first_name`, `address`, `mobile_number`, `email_address`, `username`, `password`) VALUES
 (1, 'Admin', 'admin', 'admin', 'luhdfgfkh', '0912345678', 'ndsjx@gffjd', 'admin', 'admin123'),
-(4, 'Logistics', 'Virgo', 'Sunny', 'Tobias Fornier, Antique', '0963547343', 'gfd23@gmail.com', 'sunny', 'virgo');
+(4, 'Sales', 'Virgo', 'Sunny', 'Tobias Fornier, Antique', '0963547343', 'gfd23@gmail.com', 'sunny', 'virgo'),
+(6, 'Sales', 'Zenin', 'Inumaki', 'jjk', '7564564654', 'hfdsgh@gjdfjgf.com', 'inu', 'maki');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `sale`
+--
+ALTER TABLE `sale`
+  ADD PRIMARY KEY (`sale_id`);
 
 --
 -- Indexes for table `user`
@@ -108,10 +126,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `sale`
+--
+ALTER TABLE `sale`
+  MODIFY `sale_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
